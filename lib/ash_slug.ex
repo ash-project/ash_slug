627a3d4480d256c3ac2d5813036861a6ddfcc07d
Rolf Håvard Blindheim <rolf.havard.blindheim@intility.no>
2024-05-28 14:19:41 +0200

    add: work on changes instead of separate section

--- lib/ash_slug.ex
+++ lib/ash_slug.ex
@@ -2,27 +2,5 @@ defmodule AshSlug do
   @moduledoc """
   An extension for slugifying attributes on a resource.
   """
-
-  @transformers [
-    AshSlug.Transformers.SetupSlug
-  ]
-
-  @slug %Spark.Dsl.Section{
-    name: :slug,
-    describe: "Slugify attributes of a resource",
-    schema: [
-      attributes: [
-        doc: "The attribute or attributes to slugify.",
-        type: {:wrap_list, :atom},
-        default: []
-      ],
-      options: [
-        doc: "Keyword list options to pass to `Slugify.slugify/2`",
-        type: :keyword_list,
-        default: []
-      ]
-    ]
-  }
-
-  use Spark.Dsl.Extension, sections: [@slug], transformers: @transformers
+  use Spark.Dsl.Extension, imports: [AshSlug.Changes]
 end
--- lib/ash_slug/changes/slugify.ex
+++ lib/ash_slug/changes/slugify.ex
@@ -2,22 +2,68 @@ defmodule AshSlug.Changes.Slugify do
   @moduledoc false
   use Ash.Resource.Change
 
+  @opt_schema [
+    attribute: [
+      doc: "The attribute to slugify.",
+      required: true,
+      type: :atom
+    ],
+    into: [
+      doc: "The attribute to store the slug in. Unless specified, the slug will be stored in the same attribute.",
+      type: :atom,
+      required: false
+    ],
+    lowercase?: [
+      doc: "Whether to lowercase the slug.",
+      type: :boolean,
+      default: true
+    ],
+    separator: [
+      doc: "The separator to use between words in the slug.",
+      type: :string,
+      default: "-"
+    ],
+    truncate: [
+      doc: "Truncates the slug at this character length, shortened to the nearest word.",
+      type: :integer,
+      required: false
+    ],
+    ignore: [
+      doc: "A string or list of strings of characters to ignore when slugifying.",
+      type: {:wrap_list, :string},
+      default: []
+    ]
+  ]
+
+  def opt_schema, do: @opt_schema
+
+  @impl true
+  def init(opts) do
+    case Spark.Options.validate(opts, opt_schema()) do
+      {:ok, opts} -> {:ok, replace_lowercase_opts(opts)}
+      {:error, error} -> {:error, Exception.message(error)}
+    end
+  end
+
+  @impl true
   def change(changeset, opts, _) do
     Ash.Changeset.before_action(changeset, fn changeset ->
-      attribute = opts[:field]
-      slug_opts = opts[:opts]
-
-      with {:ok, value} when is_binary(value) <- Ash.Changeset.fetch_argument(changeset, attribute),
-           slug when is_binary(slug) <- Slug.slugify(value, slug_opts) do
+      with {attribute, opts} <- Keyword.pop(opts, :attribute),
+           {into, opts} <- Keyword.pop(opts, :into, attribute),
+           {:ok, value} when is_binary(value) <- Ash.Changeset.fetch_argument_or_change(changeset, attribute),
+           slug when is_binary(slug) <- Slug.slugify(value, opts) do
         changeset
-        |> Ash.Changeset.force_change_attribute(attribute, slug)
+        |> Ash.Changeset.force_change_attribute(into, slug)
       else
-        {:ok, _} ->
-          Ash.Changeset.add_error(changeset, field: attribute, message: "is not a string value")
-
-        :error ->
-          changeset
+        {:ok, _} -> Ash.Changeset.add_error(changeset, field: opts[:attribute], message: "is not a string value")
+        :error -> changeset
       end
     end)
+  end
+
+  @spec replace_lowercase_opts(Keyword.t()) :: Keyword.t()
+  defp replace_lowercase_opts(opts) do
+    Keyword.pop(opts, :lowercase?)
+    |> then(fn {lowercase, opts} -> Keyword.put(opts, :lowercase, lowercase) end)
   end
 end
